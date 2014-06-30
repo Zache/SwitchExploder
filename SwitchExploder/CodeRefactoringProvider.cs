6c3f8b4312c784f34958e4ad4023a40c0f79ee23
Zache <zacharias.bjorngren@gmail.com>
2014-06-30 09:48:33 +0200

    Only explode selected switch, and only explode switch on Enum

--- SwitchExploder/CodeRefactoringProvider.cs
+++ SwitchExploder/CodeRefactoringProvider.cs
@@ -1,16 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeRefactorings;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Formatting;
-using Microsoft.CodeAnalysis.Options;
-using Microsoft.CodeAnalysis.Rename;
 using Microsoft.CodeAnalysis.Text;
 
 namespace SwitchExploder
@@ -24,30 +20,43 @@ namespace SwitchExploder
 		{
 			var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
 			var node = root.FindNode(textSpan);
-
-			// Only offer a refactoring if the selected node is a type declaration node.
-			var switchStatement = node as SwitchStatementSyntax;
-			if (switchStatement == null)
+			var switchStatementNode = node as SwitchStatementSyntax;
+			if (switchStatementNode == null)
 				return null;
-			
-			// For any type declaration node, create a code action to reverse the identifier text.
-			var action = CodeAction.Create("Explode Switch", c => ExplodeSwitch(document, switchStatement, c));
 
-			// Return this code action.
-			return new[] { action };
-		}
-
-		private async Task<Document> ExplodeSwitch(Document document, SwitchStatementSyntax node, CancellationToken cancellationToken)
-		{
-			var tree = await document.GetSyntaxTreeAsync(cancellationToken).ConfigureAwait(false);
-			var root = await tree.GetRootAsync(cancellationToken).ConfigureAwait(false);
 			var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
 
-			var switchRewriter = new SwitchSyntaxRewriter(semanticModel);
-			var newRoot = switchRewriter.Visit(root);
+			var memberEx = switchStatementNode.Expression as MemberAccessExpressionSyntax;
+			if (memberEx == null)
+				return null;
 
-			var newDocument = document.WithSyntaxRoot(newRoot);
-            return await Formatter.FormatAsync(newDocument, null, cancellationToken);
+			var symbolInfo = semanticModel.GetTypeInfo(memberEx.Name);
+			var enumTypeInfo = symbolInfo.Type;
+			if (enumTypeInfo.TypeKind != TypeKind.Enum)
+				return null;
+
+			var enumName = enumTypeInfo.Name;
+			var nameSpace = enumTypeInfo.ContainingNamespace.Name;
+			var enumType = Type.GetType(nameSpace + "." + enumName);
+			if (enumType == null)
+				return null;
+
+			return new[] { CodeAction.Create("Explode Switch", c => ExplodeSwitch(document, root, semanticModel, switchStatementNode, c)) };
+		}
+
+		private async Task<Document> ExplodeSwitch(Document document, 
+			SyntaxNode root, 
+			SemanticModel semanticModel, 
+			SwitchStatementSyntax node, 
+			CancellationToken cancellationToken)
+		{
+			var switchRewriter = new SwitchSyntaxRewriter(semanticModel);
+			var newNode = switchRewriter.VisitSwitchStatement(node);
+
+			root = root.ReplaceNode(node, newNode);
+			document = document.WithSyntaxRoot(root);
+
+			return await Formatter.FormatAsync(document, null, cancellationToken);
 		}
 	}
 }
